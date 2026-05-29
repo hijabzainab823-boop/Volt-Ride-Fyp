22eaca235065bb5e4ca2cb7e1ff6bad7e804a5c1
Hijab Zainab <hijabzainab823@gmail.com>
2026-05-29 14:47:55 +0500

    Updated API URL configuration

--- src/utils/ApiUrl.jsx
+++ src/utils/ApiUrl.jsx
@@ -1,3 +1,6 @@
+const API_URL = "http://localhost:8000/api";
+export default API_URL;
 export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://volt-backend-tau.vercel.app/api";
 export const FILE_URL = import.meta.env.VITE_FILE_URL || "https://volt-backend-tau.vercel.app";
 export const STRIPE_PUBLIC_KEY = "pk_test_51TWXDy44FNc1DPheKOulyA7vct2vl1dB5lPIN81XnUk0Zvs82SpB70xp7HoiACqZOwVpggvmz5HUuV4STYsJdIgc00HHQiqdH4";
+
